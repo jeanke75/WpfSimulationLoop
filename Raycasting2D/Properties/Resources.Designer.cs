e42278b42449cc53bfece57210ad0100fb824dae
jeanke75 <tom_janssen_007@hotmail.com>
2019-06-11 15:54:09 +0200

    Upagrade to .NET Framework 4.8 (forgot these T_T)

--- Raycasting2D/Properties/Resources.Designer.cs
+++ Raycasting2D/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Raycasting2D.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources
@@ -42,7 +42,7 @@ namespace Raycasting2D.Properties
         {
             get
             {
-                if ((resourceMan == null))
+                if (object.ReferenceEquals(resourceMan, null))
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Raycasting2D.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
