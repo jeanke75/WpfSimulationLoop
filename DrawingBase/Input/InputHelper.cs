e9d01980b46b6c1c0fd39bb1ccc120cd6e5600ef
jeanke75 <tom_janssen_007@hotmail.com>
2019-06-12 17:37:21 +0200

    Added a keyboardhelper

--- DrawingBase/Input/InputHelper.cs
+++ DrawingBase/Input/InputHelper.cs
@@ -11,10 +11,12 @@
     public static class InputHelper
     {
         public static readonly MouseHelper Mouse = new MouseHelper();
+        public static readonly KeyboardHelper Keyboard = new KeyboardHelper();
 
         public static void Update()
         {
             Mouse.Update();
+            Keyboard.Update();
         }
     }
 }
---
+++ DrawingBase/Input/KeyBoardHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace DrawingBase.Input
+{
+    public sealed class KeyboardHelper
+    {
+        private readonly Dictionary<Key, KeyStates> prevKeyStates;
+        private readonly Dictionary<Key, KeyStates> currentKeyStates;
+
+        public KeyboardHelper()
+        {
+            prevKeyStates = new Dictionary<Key, KeyStates>();
+            currentKeyStates = new Dictionary<Key, KeyStates>();
+            var x = (Key[])Enum.GetValues(typeof(Key));
+            foreach (Key k in x)
+            {
+                // Test if the key is valid
+                try
+                {
+                    var state = Keyboard.GetKeyStates(k);
+                    prevKeyStates.Add(k, state);
+                    currentKeyStates.Add(k, state);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+
+        public void Update()
+        {
+            foreach (Key k in new List<Key>(currentKeyStates.Keys))
+            {
+                prevKeyStates[k] = currentKeyStates[k];
+                currentKeyStates[k] = Keyboard.GetKeyStates(k);
+            }
+        }
+
+        public ButtonState GetPressedState(Key key)
+        {
+            return GetPressedState(prevKeyStates[key], currentKeyStates[key]);
+        }
+
+        private ButtonState GetPressedState(KeyStates prevKeyState, KeyStates currentKeyState)
+        {
+            if (!prevKeyState.HasFlag(KeyStates.Down))
+            {
+                if (currentKeyState.HasFlag(KeyStates.Down))
+                    return ButtonState.Pressed;
+                else
+                    return ButtonState.Up;
+            }
+            else
+            {
+                if (currentKeyState.HasFlag(KeyStates.Down))
+                    return ButtonState.Down;
+                else
+                    return ButtonState.Released;
+            }
+        }
+    }
+}
