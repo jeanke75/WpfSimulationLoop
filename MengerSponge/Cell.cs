7b5a1c64d7cdf7dd24f7f0fe14c3bfe62ba9c1fc
tomjan <tom.janssen@cegeka.com>
2019-07-05 13:09:48 +0200

    Menger Sponge added

---
+++ MengerSponge/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MengerSponge
+{
+    readonly struct Cell
+    {
+        public readonly Rect rect;
+
+        public Cell(double x, double y, double size)
+        {
+            rect = new Rect(x, y, size, size);
+        }
+
+        public void Draw(DrawingContext dc, Brush fillBrush, Pen outlinePen)
+        {
+            dc.DrawRectangle(fillBrush, outlinePen, rect);
+        }
+    }
+}
---
+++ MengerSponge/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using DrawingBase;
+using DrawingBase.Input;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MengerSponge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DrawingWindowBase
+    {
+        public static readonly Random random = new Random();
+        private List<Cell> sponge = new List<Cell>();
+        private readonly Brush cellBrush = Brushes.Gray;
+
+        private readonly KeyboardHelper keyboardHelper = new KeyboardHelper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public override void Initialize()
+        {
+            SetResolution(600, 600);
+            sponge.Add(new Cell(0, 0, 600));
+            cellBrush.Freeze();
+        }
+
+        public override void Update(float dt)
+        {
+            keyboardHelper.Update();
+
+            if (keyboardHelper.GetPressedState(Key.Space) == ButtonState.Pressed)
+            {
+                var newSponge = new List<Cell>();
+                double newSize = sponge[0].rect.Width / 3d;
+                foreach (Cell c in sponge)
+                {
+                    // Subdivide
+                    for (int x = 0; x < 3; x++)
+                    {
+                        for (int y = 0; y < 3; y++)
+                        {
+                            // Leave the middle cell open
+                            if (x != 1 || y != 1)
+                            {
+                                newSponge.Add(new Cell(c.rect.X + x * newSize, c.rect.Y + y * newSize, newSize));
+                            }
+                        }
+                    }
+                }
+                sponge.Clear();
+                sponge = newSponge;
+            }
+        }
+
+        public override void Draw(DrawingContext dc)
+        {
+            foreach (Cell c in sponge)
+            {
+                c.Draw(dc, cellBrush, null);
+            }
+        }
+
+        public override void Cleanup()
+        {
+        }
+    }
+}
