f88faf43bef1e55fd1225427c0cafebd88de8806
tomjan <tom.janssen@cegeka.com>
2019-08-22 12:02:29 +0200

    Forgot to commit this file

--- AoE/UI/SelectionPanel.cs
+++ AoE/UI/SelectionPanel.cs
@@ -11,7 +11,7 @@ namespace AoE.UI
 {
     class SelectionPanel
     {
-        private readonly Rect rect;
+        private readonly Vector panelOffset;
 
         private readonly CultureInfo cultureInfo;
         private readonly FlowDirection flowDirection;
@@ -21,7 +21,7 @@ namespace AoE.UI
 
         public SelectionPanel(DrawingWindowBase window)
         {
-            rect = new Rect(0, window.GetHeight() - 90, window.GetWidth(), 100);
+            panelOffset = new Vector(89, window.GetHeight() - 90);
 
             cultureInfo = CultureInfo.CurrentCulture;
             flowDirection = FlowDirection.LeftToRight;
@@ -40,14 +40,14 @@ namespace AoE.UI
 
                 // Draw info
                 var nameText = new FormattedText(selectedBaseGameObject.Name, cultureInfo, flowDirection, typeface, 12d, foregroundBrush, pixelsPerDip);
-                dc.DrawText(nameText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                dc.DrawText(nameText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
 
                 yOffset += nameText.Height;
 
                 if (selectedBaseGameObject is IDestroyable selectedDestroyable)
                 {
                     var hitpointText = new FormattedText($"Hitpoints: {selectedDestroyable.GetHitPoints()}/{selectedDestroyable.GetHitPointsMax()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                    dc.DrawText(hitpointText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                    dc.DrawText(hitpointText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                     yOffset += hitpointText.Height;
 
                     if (selectedDestroyable is ICombat selectedCombat)
@@ -55,31 +55,31 @@ namespace AoE.UI
                         if (selectedCombat.GetMeleeAttack() > 0)
                         {
                             var meleeAttackText = new FormattedText($"Melee attack: {selectedCombat.GetMeleeAttack()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                            dc.DrawText(meleeAttackText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                            dc.DrawText(meleeAttackText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                             yOffset += meleeAttackText.Height;
                         }
                         if (selectedCombat.GetPierceAttack() > 0)
                         {
                             var pierceAttackText = new FormattedText($"Pierce attack: {selectedCombat.GetPierceAttack()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                            dc.DrawText(pierceAttackText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                            dc.DrawText(pierceAttackText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                             yOffset += pierceAttackText.Height;
                         }
                         if (selectedCombat.GetBlastRadius() > 0)
                         {
                             var blastRadiusText = new FormattedText($"Blast radius: {selectedCombat.GetBlastRadius()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                            dc.DrawText(blastRadiusText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                            dc.DrawText(blastRadiusText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                             yOffset += blastRadiusText.Height;
                         }
                         if (selectedCombat.GetMeleeArmor() > 0)
                         {
                             var meleeArmorText = new FormattedText($"Melee armor: {selectedCombat.GetMeleeArmor()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                            dc.DrawText(meleeArmorText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                            dc.DrawText(meleeArmorText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                             yOffset += meleeArmorText.Height;
                         }
                         if (selectedCombat.GetPierceArmor() > 0)
                         {
                             var pierceArmorText = new FormattedText($"Pierce armor: {selectedCombat.GetPierceArmor()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                            dc.DrawText(pierceArmorText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                            dc.DrawText(pierceArmorText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                             yOffset += pierceArmorText.Height;
                         }
 
@@ -88,7 +88,7 @@ namespace AoE.UI
                             if (selectedRangedCombat.GetAttackRangeMax() > 0)
                             {
                                 var rangeText = new FormattedText($"Range: {selectedRangedCombat.GetAttackRangeMin()} - {selectedRangedCombat.GetAttackRangeMax()}", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                                dc.DrawText(rangeText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                                dc.DrawText(rangeText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                                 yOffset += rangeText.Height;
                             }
                         }
@@ -98,21 +98,21 @@ namespace AoE.UI
                 if (selectedBaseGameObject is IActionable selectedActionable && selectedActionable.GetAction() is Gather selectedActionableGatherAction && !selectedActionableGatherAction.Completed())
                 {
                     var gatherActionText = new FormattedText($"Gathering: {selectedActionableGatherAction.AmountCarried}/{selectedActionableGatherAction.AmountCarriedMax} ({selectedActionableGatherAction.Resource.Type.ToString()})", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                    dc.DrawText(gatherActionText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                    dc.DrawText(gatherActionText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                     yOffset += gatherActionText.Height;
                 }
 
                 if (selectedBaseGameObject is BaseResource selectedBaseResource)
                 {
                     var hitpointText = new FormattedText($"Resources: {selectedBaseResource.Amount}/{selectedBaseResource.AmountMax} ({selectedBaseResource.Type.ToString()})", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                    dc.DrawText(hitpointText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                    dc.DrawText(hitpointText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                     yOffset += hitpointText.Height;
                 }
 
                 if (selectedBaseGameObject is IConstructable selectedConstructable && selectedConstructable.GetConstructionTime() > 0)
                 {
                     var constructionTimeText = new FormattedText($"Constructing: {((selectedConstructable.GetConstructionTimeTotal() - selectedConstructable.GetConstructionTime()) / selectedConstructable.GetConstructionTimeTotal() * 100).ToString("0.00")}% ", cultureInfo, flowDirection, typeface, 10d, foregroundBrush, pixelsPerDip);
-                    dc.DrawText(constructionTimeText, new Point(rect.X + xOffset, rect.Y + yOffset));
+                    dc.DrawText(constructionTimeText, new Point(panelOffset.X + xOffset, panelOffset.Y + yOffset));
                     //yOffset += hitpointText.Height;
                 }
             }
